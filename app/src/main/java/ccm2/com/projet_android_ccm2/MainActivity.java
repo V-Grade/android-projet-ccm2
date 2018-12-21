fe9bce08f6b76e820911c3632bfe64f48d83acd6
Azerka <mail@azerka.fr>
2018-12-21 14:56:32 +0100

    First commit with new android project

---
+++ app/src/main/java/ccm2/com/projet_android_ccm2/MainActivity.java
@@ -0,0 +1,13 @@
+package ccm2.com.projet_android_ccm2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
